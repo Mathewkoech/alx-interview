0820136829bc3b5ac74c28e6a1a48dda1861a50e
mathewkoech <mathewkoech55@gmail.com>
2024-09-19 17:06:22 +0300

    pycodestyle validation

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
 Making Change"""
+
+
 def makeChange(coins, total):
-    """Given a pile of coins of different values, 
-    determine the fewest number of coins needed to meet a given amount total."""
+    """Given a pile of coins of different values,
+    determine the fewest number of
+    coins needed to meet a given amount total."""
     if total <= 0:
         return 0
-    coins.sort(reverse=True) # sort coins in descending order
+    coins.sort(reverse=True)  # sort coins in descending order
     change = 0
-    for coin in coins: # iterate through coins
+    for coin in coins:   # iterate through coins
         while total >= coin:
             total -= coin
             change += 1
     if total != 0:
         return -1
     return change
-
