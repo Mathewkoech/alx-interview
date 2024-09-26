353feb0cc7f0090eceaadd8e1b96e235713339ce
mathewkoech <mathewkoech55@gmail.com>
2024-09-26 22:45:07 +0300

    documenting

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Island Perimeter Module
+"""Island Perimeter Module"""
 def island_perimeter(grid):
     """returns the perimeter of the island described in grid"""
     perimeter = 0
